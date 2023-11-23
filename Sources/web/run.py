f77ad7e6c9d99bd48a611139ca4c44d7f8f2b83c
MarcinNowak94 <marcin94nowak@gmail.com>
2023-11-23 20:05:04 +0100

    Dockerized application

--- Sources/web/run.py
+++ Sources/web/run.py
@@ -1,7 +1,10 @@
 from RightenWeb import app
 import ssl
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain('E:\Projects\Git\Righten\Sources\web\cert.pem', 'E:\Projects\Git\Righten\Sources\web\key.pem')
+#TODO: Change this so it works alsoe within container
+context.load_cert_chain('/righten/cert.pem', '/righten/key.pem')
+#context.load_cert_chain('E:\Projects\Git\Righten\Sources\web\cert.pem', 'E:\Projects\Git\Righten\Sources\web\key.pem')
 
 if __name__=="__main__":
-    app.run(ssl_context=context)
+    app.run(host="0.0.0.0", port=5000, ssl_context=context)
+    #app.run(ssl_context=context)
