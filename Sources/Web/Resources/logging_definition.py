44a1da192d9f691a602fcab69aae874151a9ca0f
MarcinNowak94 <marcin94nowak@gmail.com>
2024-03-01 18:12:47 +0100

    Logging - first working version

--- Sources/Web/Resources/logging_definition.py
+++ Sources/Web/Resources/logging_definition.py
@@ -3,6 +3,7 @@ import atexit
 import json
 import logging.config
 import logging.handlers
+from logging import FileHandler
 import pathlib
 from Resources.rightenlogger import RightenJSONFormatter
 
@@ -10,7 +11,55 @@ from Resources.rightenlogger import RightenJSONFormatter
 
 logger = logging.getLogger("righten") #Create non-rootlogger if not exists, 
 
+logging_config={
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "%(actime)s, %(levelname)s, %(message)s",
+            "datefmt": "%Y-%m-%dT%H:%M:%S%z"
+        },
+        "detailed": {
+            "format": "%(asctime)s, %(levelname)s, %(module)s, L%(lineno)d: %(message)s",
+            "datefmt": "%Y-%m-%dT%H:%M:%S%z"
+        }
+    },
+    "handlers": {
+        "stdout": {
+            "class": "logging.StreamHandler",
+            "formatter": "detailed",
+            "stream": "ext://sys.stdout"
+        },
+        "stderr": {
+            "class": "logging.StreamHandler",
+            "level": "WARNING",
+            "formatter": "detailed",
+            "stream": "ext://sys.stderr"
+        },
+        "queue_handler": {
+            "class": "logging.handlers.QueueHandler",
+            "handlers": [
+                "stdout"
+            ],
+            "respect_handler_level": True
+        }
+    },
+    "loggers": {
+        "root": {
+            "level": "DEBUG",
+            "handlers": ["queue_handler"]
+        }
+    }
+}
+
 def setup_logging():
+    logging.config.dictConfig(config=logging_config)
+    queue_handler=logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
+
+def setup_logging_v2():
     config_file = pathlib.Path("E:\Projects\Git\Righten\Sources\Web\Resources\logging_config.json")
     with open(config_file) as file:
         config=json.load(file)
